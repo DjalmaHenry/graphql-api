1fe0b9c3027f66762fea411b223488abfd7547b6
Djalma Henrique <contato@djalmahenry.com>
2022-11-03 23:12:21 -0300

    feat: add customers list

--- frontend/src/pages/Customers/Customers.js
+++ frontend/src/pages/Customers/Customers.js
@@ -10,20 +10,30 @@ import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
 import { Button } from "@mui/material";
+import { gql, useQuery } from "@apollo/client";
 
-function createData(name, calories, fat, carbs, protein) {
-  return { name, calories, fat, carbs, protein };
+function createData(name, id) {
+  return { name, id };
 }
 
-const rows = [
-  createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
-  createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
-  createData("Eclair", 262, 16.0, 24, 6.0),
-  createData("Cupcake", 305, 3.7, 67, 4.3),
-  createData("Gingerbread", 356, 16.0, 49, 3.9),
-];
+const GET_CUSTOMERS = gql`
+  query {
+    customers {
+      id
+      first_name
+    }
+  }
+`;
 
 export function Customers() {
+  const { data } = useQuery(GET_CUSTOMERS);
+  const rows = data?.customers.map((customer) => {
+    return createData(
+      customer.first_name,
+      customer.id
+    );
+  });
+
   return (
     <div>
       <Navbar />
@@ -31,7 +41,7 @@ export function Customers() {
         <Table sx={{ minWidth: 650 }} aria-label="simple table">
           <TableHead>
             <TableRow>
-              <TableCell>Company Name</TableCell>
+              <TableCell>Customer Name</TableCell>
               <TableCell align="right"></TableCell>
               <TableCell align="right"></TableCell>
               <TableCell align="right"></TableCell>
